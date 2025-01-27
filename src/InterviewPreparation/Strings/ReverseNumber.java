0e000e35ae823dfaf3b612795f4f39c602294287
jsatheesh568 <jsatheesh568@gmail.com>
2025-01-27 07:26:07 +0530

    add: without functional logic

--- src/InterviewPreparation/Strings/ReverseNumber.java
+++ src/InterviewPreparation/Strings/ReverseNumber.java
@@ -11,4 +11,15 @@ public class ReverseNumber {
     private static int reverseNumber ( int number ) {
         return Integer.parseInt(new StringBuilder(String.valueOf(number)).reverse().toString());
     }
+
+/*   // Reverse the number without using inbuilt function.
+ private static int reverseNumber(int num) {
+        int reversed = 0;
+        while (num != 0) {
+            int digit = num % 10;  // Extract the last digit
+            reversed = reversed * 10 + digit; // Append the digit to the reversed number
+            num /= 10; // Remove the last digit
+        }
+        return reversed;
+    }*/
 }
