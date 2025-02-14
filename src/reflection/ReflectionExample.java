e486dc1190a25599e65140cc3132367bd5b28e8f
jsatheesh568 <jsatheesh568@gmail.com>
2025-02-14 11:58:10 +0530

    java reflections

---
+++ src/reflection/ReflectionExample.java
@@ -0,0 +1,50 @@
+package CoreJavaPractices.src.reflection;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+import java.lang.reflect.Constructor;
+
+class Employee {
+    private String name;
+    private int age;
+
+    public Employee() {}
+
+    public void display() {
+        System.out.println("Employee Details");
+    }
+}
+
+public class ReflectionExample {
+    public static void main(String[] args) {
+        try {
+            // Get Class object
+            Class<?> obj = Class.forName("CoreJavaPractices.src.reflection.Employee");
+
+            // Get class name
+            System.out.println("Class Name: " + obj.getName());
+
+            // Get methods
+            Method[] methods = obj.getDeclaredMethods();
+            for (Method method : methods) {
+                System.out.println("Method: " + method.getName());
+            }
+
+            // Get fields
+            Field[] fields = obj.getDeclaredFields();
+            for (Field field : fields) {
+                System.out.println("Field: " + field.getName());
+            }
+
+            // Get constructors
+            Constructor<?>[] constructors = obj.getDeclaredConstructors();
+            for (Constructor<?> constructor : constructors) {
+                System.out.println("Constructor: " + constructor.getName());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
