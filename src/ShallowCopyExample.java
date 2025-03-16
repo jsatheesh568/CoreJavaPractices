4d93c2efbe731be5b4d08369b3e1d92428891469
jsatheesh568 <jsatheesh568@gmail.com>
2025-03-16 10:05:59 +0530

    shallowcopy example

---
+++ src/ShallowCopyExample.java
@@ -0,0 +1,45 @@
+package CoreJavaPractices.src;
+
+/*
+Copies only references, not the actual objects.
+Changes in the cloned object's reference type fields reflect in the original object.
+Uses super.clone() for copying.
+Faster but may lead to unintended modifications
+ */
+class Address {
+  String city;
+
+  Address(String city) {
+    this.city = city;
+  }
+}
+
+class ShallowCopyExample implements Cloneable {
+  String name;
+  Address address;
+
+  ShallowCopyExample(String name, Address address) {
+    this.name = name;
+    this.address = address;
+  }
+
+  @Override
+  protected Object clone() throws CloneNotSupportedException {
+    return super.clone(); // Shallow copy
+  }
+
+  public static void main(String[] args) throws CloneNotSupportedException {
+    Address addr = new Address("New York");
+    ShallowCopyExample person1 = new ShallowCopyExample("John", addr);
+
+    // Creating a shallow copy
+    ShallowCopyExample person2 = (ShallowCopyExample) person1.clone();
+
+    // Changing cloned object's address
+    person2.address.city = "Los Angeles";
+
+    // Output results
+    System.out.println("Person 1 Address: " + person1.address.city); // Output: Los Angeles
+    System.out.println("Person 2 Address: " + person2.address.city); // Output: Los Angeles
+  }
+}
